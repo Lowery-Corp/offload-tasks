be0524ff711e6a6cd35e1e2450c053fcc2b21bc9
JMGrubbs <johnmlowery@gmail.com>
2026-04-20 20:55:13 +0000

    pulling files frim minio in offload api.

--- api/v1/endpoints/file_ingestion.py
+++ api/v1/endpoints/file_ingestion.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, Depends
 from auth.dependencies import authenticate_request
 from schemas.user import AuthorizedUser
 from schemas.file import NewFileIngestionTask
+from repositories.minio import get_file_from_minio
 
 router = APIRouter(tags=["ingestion"])
 
@@ -12,10 +13,17 @@ async def offload_file_ingestion_task(
     file_metadata: NewFileIngestionTask,
     authorized_user: AuthorizedUser = Depends(authenticate_request),
 ) -> dict[str, str | bool]:
-
     print(f"Received file ingestion request for user {authorized_user.username} (ID: {authorized_user.id})", flush=True)
     print(f"Is admin: {authorized_user.is_admin}", flush=True)
     print(f"File metadata: {file_metadata}", flush=True)
+
+    bucket_name = f"""user-{file_metadata.user_id}-bucket"""
+
+    user_file = await get_file_from_minio(bucket_name=bucket_name, file_path=file_metadata.storage_key)
+
+    print(user_file.keys())
+
+
 
     print("Offloading file ingestion task to Celery...", flush=True)
     return {"message": "File ingestion task offloaded to Celery", "ok": True}
